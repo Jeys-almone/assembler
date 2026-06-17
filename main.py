6abfe283618e5c54f746c580644f816dfa0c74cd
Jeys Almone <jeys532667@gmail.com>
2026-06-17 10:51:53 -0300

    feat(main): implementa segunda passagem e geracao do arquivo hack

--- main.py
+++ main.py
@@ -2,28 +2,67 @@ import sys
 
 from parser.parser import Parser
 from symbol_table.symbol_table import SymbolTable
+from code_generator.code import encode_a_instruction, encode_c_instruction
 
 
-def main():
-    input_file = sys.argv[1]
-
+def first_pass(input_file, symbols):
     parser = Parser(input_file)
-    symbols = SymbolTable()
-
     address = 0
 
     while parser.has_more_instructions():
         parser.advance()
 
         if parser.instruction_type() == "LABEL":
-            symbols.add_entry(
-                parser.symbol(),
-                address
-            )
+            symbols.add_entry(parser.symbol(), address)
         else:
             address += 1
 
-    print(symbols.symbols)
+
+def second_pass(input_file, output_file, symbols):
+    parser = Parser(input_file)
+
+    with open(output_file, "w", encoding="utf-8") as output:
+        while parser.has_more_instructions():
+            parser.advance()
+
+            if parser.instruction_type() == "LABEL":
+                continue
+
+            if parser.instruction_type() == "A_INSTRUCTION":
+                symbol = parser.symbol()
+
+                if symbol.isdigit():
+                    binary = encode_a_instruction(symbol)
+                else:
+                    address = symbols.add_variable(symbol)
+                    binary = encode_a_instruction(str(address))
+
+                output.write(binary + "\n")
+
+            elif parser.instruction_type() == "C_INSTRUCTION":
+                binary = encode_c_instruction(
+                    parser.comp(),
+                    parser.dest(),
+                    parser.jump()
+                )
+
+                output.write(binary + "\n")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Uso: python main.py arquivo.asm")
+        return
+
+    input_file = sys.argv[1]
+    output_file = input_file.replace(".asm", ".hack")
+
+    symbols = SymbolTable()
+
+    first_pass(input_file, symbols)
+    second_pass(input_file, output_file, symbols)
+
+    print(f"Arquivo gerado: {output_file}")
 
 
 if __name__ == "__main__":
