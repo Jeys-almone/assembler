ad1e8e243fed68b5f32dbcc1131cf5e753ce662a
Jeys Almone <jeys532667@gmail.com>
2026-06-17 09:51:26 -0300

    feat(parser): implementa leitura e navegação de instruções

---
+++ parser/parser.py
@@ -0,0 +1,23 @@
+class Parser:
+    def __init__(self, filename):
+        self.lines = []
+        self.index = 0
+        self.current_instruction = None
+
+        with open(filename, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.split("//")[0].strip()
+
+                if line:
+                    self.lines.append(line)
+
+    def has_more_instructions(self):
+        return self.index < len(self.lines)
+
+    def advance(self):
+        if self.has_more_instructions():
+            self.current_instruction = self.lines[self.index]
+            self.index += 1
+            return self.current_instruction
+
+        return None
